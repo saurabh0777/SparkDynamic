fb47e3e0eca52dd196d0f393acdcb9f3de71430d
Saurabh Srivastava <30717094+saurabh0777@users.noreply.github.com>
2019-02-03 21:34:57 +0800

    OracleFetch
    Code is used to fetch the data from oracle as a CSV and convert to parquet using spark.

---
+++ OracleFetch.scala
@@ -0,0 +1,31 @@
+package com.test.poc
+
+import org.apache.spark.sql.SparkSession
+
+    object PullOrcale {
+      
+  def main(args: Array[String]): Unit = {
+      
+    val spark = SparkSession.builder()
+          .appName("ConnectingOracleDatabase")
+          .master("local[*]")
+          .getOrCreate()
+
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:oracle:thin:@10.100.65.36:1521/CVMDBP")
+      .option("dbtable", "MINING.CAR_PR_SUB_CSV_20181031")
+      .option("user", "XYZEG")
+      .option("password", "FAU#rf")
+      .option("driver", "oracle.jdbc.OracleDriver")
+      .load()
+      
+    jdbcDF.printSchema()
+
+    jdbcDF.write.mode("overwrite").option("header", "true").format("csv").save("D:/Data/output/CAR_PR_SUB_CSV_20181031/")
+    
+    
+    /*jdbcDF.coalesce(1).write.mode("append").parquet("D:/Data/output/CAR_PO_SUB_CSV_20181031")
+*/
+  }
+}
